14c0b921e5fc54d560acb90408c63990a0856124
Ashraf Mahmoud <ashraf1abdlrasool@gmail.com>
2026-01-12 17:53:03 +0300

    Set up Playwright for end-to-end testing, including initial test configuration, workflow, and example tests.

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,6 +38,7 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
+/*
 
     {
       name: 'firefox',
@@ -48,6 +49,7 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
+*/
 
     /* Test against mobile viewports. */
     // {
